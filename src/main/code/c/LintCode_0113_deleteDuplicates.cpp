bd518eb7987f4b5059f143e4857a79848ae08c62
memehead <57305869+memehead@users.noreply.github.com>
2022-12-19 09:03:29 +0800

    Update LintCode_0113_deleteDuplicates.cpp

--- src/main/code/c/LintCode_0113_deleteDuplicates.cpp
+++ src/main/code/c/LintCode_0113_deleteDuplicates.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-
 using namespace std;
 
 // https://www.lintcode.com/problem/113
-
 class ListNode {
 public:
     int val;
@@ -49,24 +47,3 @@ ListNode* deleteDuplicates_0113(ListNode* head) {
 
     return dummmy->next;
 }
-
-//int main() {
-//	ListNode* node1 = new ListNode(1);
-//	ListNode* node2 = new ListNode(2);
-//	ListNode* node3 = new ListNode(3);
-//	ListNode* node4 = new ListNode(3);
-//	ListNode* node5 = new ListNode(4);
-//	ListNode* node6 = new ListNode(4);
-//	ListNode* node7 = new ListNode(5);
-//
-//	node1->next = node2;
-//	node2->next = node3;
-//	node3->next = node4;
-//	node4->next = node5;
-//	node5->next = node6;
-//	node6->next = node7;
-//
-//	ListNode* ans = deleteDuplicates_0113(node1);
-//
-//	return 0;
-//}
