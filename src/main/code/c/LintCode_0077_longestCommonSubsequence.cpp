d17c39becf49f604c489f7543fc20e05d0482c68
memehead <57305869+memehead@users.noreply.github.com>
2022-12-19 09:01:14 +0800

    Update LintCode_0077_longestCommonSubsequence.cpp

--- src/main/code/c/LintCode_0077_longestCommonSubsequence.cpp
+++ src/main/code/c/LintCode_0077_longestCommonSubsequence.cpp
@@ -28,7 +28,6 @@ int longestCommonSubsequence(string& a, string& b) {
 			dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
 			
 			if (a.at(i) == b.at(j)) {
-				//可以优化
 				//dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
 				dp[i][j] = dp[i - 1][j - 1] + 1;
 			}
